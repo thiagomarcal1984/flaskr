0b6863993ace6d3d74981cd0f0e28cb6a7e3d796
Thiago Marçal <thiagomarcal1984@gmail.com>
2021-03-25 20:19:10 -0300

    Fim da página Application Setup.

---
+++ flaskr/__init__.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # Cria e configura o app.
+    app = Flask(__name__, instance_relative_config=True) 
+    # instance_relative_config=True muda a referência da instância: 
+    # False (padrão) é relativo ao arquivo.py; 
+    # True se refere à pasta onde o __init__ é declarado.
+    app.config.from_mapping(
+        SECRET_KEY = b'-6\xe3\\[\xee\xc1\x8cl\x17r\xf2\xf8\xd3\x14\x0b',
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+    
+    if test_config is None:
+        # Carrega o arquivo de configuração, se houver, quando não está testando.
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Carrega a configuração de teste
+        app.config.from_mapping(test_config)
+    
+    # Garante que a pasta da instância existe, para cirar o DB.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+        
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world!'
+        
+    return app
